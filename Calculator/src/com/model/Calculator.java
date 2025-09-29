9f23013bc83f13fa9743b66f51f91ec9c242759d
Yogesh_Yeole <pranjalijamale@gmail.com>
2025-09-29 20:52:54 +0530

    add calculator class

---
+++ Calculator/src/com/model/Calculator.java
@@ -0,0 +1,15 @@
+package com.model;
+
+public class Calculator {
+	
+	int a, b;
+	
+	public void add() {
+		
+		a=20;
+		b=30;
+				
+		System.out.println(a+b);
+	}
+
+}
--- Calculator/src/com/model/Test.java
+++ Calculator/src/com/model/Test.java
@@ -4,7 +4,8 @@ public class Test {
 	
 	public static void main(String[] args) {
 		
-		
+		Calculator c = new Calculator();
+		c.add();
 	}
 
 }
